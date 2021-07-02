da8ba4c127a1f02144152d58301a7e0bb021fea4
aryan251211 <81881363+aryan251211@users.noreply.github.com>
2021-07-02 19:20:05 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(500)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        led.plot(2, 2)
+    } else {
+        led.unplot(2, 2)
+    }
 })
